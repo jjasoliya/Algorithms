64764cdd0fda9c0c663b1d1140230af1ea9d59a7
jjasoliy <jaymin_jasoliya@outlook.com>
2017-03-12 20:07:50 -0700

    changed the directory structure to seperate include and src files

--- algorithms/algorithms/linkedlist.h
+++ algorithms/algorithms/linkedlist.h
@@ -3,16 +3,22 @@ class linkedlist
 {
 
 private:
-	struct linknode
+	struct node
 	{
 		int data;
-		int *linkpointer;
-	} *ln;
+		node *link;
+	} *p;
 
 public:
-	linkedlist();
-	virtual ~linkedlist();
-	void initialize()
-	void addnode(int data);
+	linkedlist();  // constructor
+	~linkedlist(); // destructor
+	void append_node(int num); // appends a node in linked list 
+	void del(int num);    // finds and deletes matching node from linkedlist
+	void addatbeg(int num);  // adds at the beginning of the linked list
+	void addafter(int c, int num);  // adds node after specified number of nodes 
+	void count();
+	void display(); // displays linked list
+
+
 };
 
