7b86dff2bbff52832f2bcc822009388f0b171917
Jaymin Jasoliya <jayminj@microsoft.com>
2022-04-10 17:10:53 -0700

    restructuring again.

---
+++ cpp_reference/cpp_ref/cpp_ref.cpp
@@ -0,0 +1,14 @@
+﻿// cpp_ref.cpp : Defines the entry point for the application.
+//
+
+#include "cpp_ref.h"
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+
+	return 0;
+}
